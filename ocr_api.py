ad33a09ced5a8e67ba08879e2469353312dc8dff
socpa124212 <socpa1234@kau.kr>
2023-06-12 00:07:05 +0900

    doc:add explanation ocr_api

--- ocr_api.py
+++ ocr_api.py
@@ -22,6 +22,7 @@ class ocr_api :
 
         ocr = Pororo(task="ocr", lang=self.config.ocr.pororo.lang)
         result = ocr(img_pth)
+
         return result
 
 
@@ -32,7 +33,10 @@ class ocr_api :
 
         reader = easyocr.Reader(self.config.ocr.easy.lang, gpu=self.config.ocr.easy.GPU)
         result = reader.readtext(img_pth)
+        # easyocr.Reader.readtext() returns list of tuple about detection
+        # form : ([coordination], result, accuracy)
         txt = [line[1] for line in result]
+
         return txt
 
 
@@ -42,8 +46,9 @@ class ocr_api :
         ) -> str:
 
         image = Image.open(img_pth)
-        text = pytesseract.image_to_string(image, lang=self.config.ocr.tesseract.lang)  # 한글 언어 설정
+        text = pytesseract.image_to_string(image, lang=self.config.ocr.tesseract.lang)
         result = text.replace('\r', '')
+
         return result
 
 
@@ -54,8 +59,9 @@ class ocr_api :
 
         ocr = PaddleOCR(use_angle_cls=True, lang=self.config.ocr.paddle.lang)
         result = ocr.ocr(img_pth, cls=True)
-        # result :좌표, 결과, 정확도
-        result = result[0]
-        txts = [line[1][0] for line in result]
+        # PaddleOCR.ocr() returns str that indicate detected text as list(size=1) 
+        # form : [[coordination], (result, accuracy)]    
+        result = result[0] # result element extraction
+        txts = [line[1][0] for line in result] # text extraction
+
         return txts
-    
--- run_ocr.py
+++ run_ocr.py
@@ -8,7 +8,7 @@ def run_ocr(config) -> None:
     ocr_instance = OCR(config = config) # config 호출
     ocr_instance.File_gen()
 
-run_ocr()
+# run_ocr()
 
-# if __name__ == "__main__":
-#     run_ocr()
+if __name__ == "__main__":
+    run_ocr()
